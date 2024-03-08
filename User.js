b17590450378752e73bbfcedf44d47448469ec70
Paul LeTourneau <pletourneau62@gmail.com>
2024-03-08 11:52:45 -0800

    update user schema

--- User.js
+++ User.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
+  coins: { type: Number, default: 1000 },
+  isAdmin: { type: Boolean, default: false },
   created_at: { type: Date, default: Date.now },
 });
 
